56eb8bf67ec514b3b0b42ac31c9c76f371e5e659
Chris Su <zerogpm@gmail.com>
2024-11-27 10:37:46 -0800

    init

---
+++ src/App.js
@@ -0,0 +1,7 @@
+function App() {
+    return (<div>
+        <h1>My React App</h1>
+    </div>);
+}
+
+export default App;
---
+++ src/api.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const searchImages = async (term) => {
+  //const response = await axios.get(`https://swapi.py4e.com/api/people/${term}/`);
+  //return response.data;
+};
+
+export default searchImages;
