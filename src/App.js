5dfe775b323a95d607aea85381b29590ed423550
Chris Su <zerogpm@gmail.com>
2024-11-27 11:25:43 -0800

    add fetch method data

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
+import { useEffect, useState } from "react";
+
 export default function App() {
-    return (
-      <div>
-        <input type="text" />
-        <select>
-          <option value="USD">USD</option>
-          <option value="EUR">EUR</option>
-          <option value="CAD">CAD</option>
-          <option value="INR">INR</option>
-        </select>
-        <select>
-          <option value="USD">USD</option>
-          <option value="EUR">EUR</option>
-          <option value="CAD">CAD</option>
-          <option value="INR">INR</option>
-        </select>
-        <p>OUTPUT</p>
-      </div>
-    );
-  }
+  const [amount, setAmount] = useState(1);
+  const [fromCurrency, setFromCurrency] = useState("CNY");
+  const [toCurrency, setToCurrency] = useState("USD");
+
+  useEffect(function () {
+    async function convert() {
+      const res = await fetch(
+        `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurrency}&to=${toCurrency}`
+      );
+      const data = await res.json();
+      console.log(data);
+    }
+    convert();
+  }, []);
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={amount}
+        onChange={(e) => setAmount(Number(e.target.value))}
+      />
+      <select
+        value={fromCurrency}
+        onChange={(e) => setFromCurrency(e.target.value)}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <select
+        value={toCurrency}
+        onChange={(e) => setToCurrency(e.target.value)}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <p>OUTPUT</p>
+    </div>
+  );
+}
